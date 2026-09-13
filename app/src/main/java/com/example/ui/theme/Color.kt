6e55ba36278a097c1434916a65a41b8d8187744c
DEEPAK KUSHWAHA <prayagideepak@gmail.com>
2026-09-13 13:16:51 +0530

    style: update branding colors and app icon assets
    - Define custom brand color palette (RoyalBlue, GoldAccent)
    - Update theme to use branded color scheme
    - Disable dynamic colors to maintain brand identity
    - Replace default launcher icons with custom design

--- app/src/main/java/com/example/ui/theme/Color.kt
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package com.example.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val RoyalBlue = Color(0xFF1A3B8B)
+val DeepNavy = Color(0xFF0B1D4B)
+val GoldAccent = Color(0xFFFFC107)
+val BrightOrange = Color(0xFFFF9800)
+val SurfaceLight = Color(0xFFF8F9FA)
+val SurfaceDark = Color(0xFF121212)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryLight = RoyalBlue
+val SecondaryLight = BrightOrange
+val TertiaryLight = GoldAccent
+
+val PrimaryDark = Color(0xFF90CAF9)
+val SecondaryDark = Color(0xFFFFB74D)
+val TertiaryDark = Color(0xFFFFE082)
